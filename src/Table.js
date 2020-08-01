186d768d1c050724778941d25c82696fa10bc6b9
Majd Sufian <majdnewsufian@gmail.com>
2020-08-01 14:59:25 +0200

    add tiltes for the table

--- src/Table.js
+++ src/Table.js
@@ -5,11 +5,23 @@ import numeral from "numeral"; // to format numbers in a certain way
 function Table({ countries }) {
   return (
     <div className="table">
-      {countries.map(({ country, cases }) => (
+      <div className="table__title">
+        <h4>Country</h4>
+        <h4>Cases</h4>
+        <h4>Recovered</h4>
+        <h4>Deaths</h4>
+      </div>
+      {countries.map((country) => (
         <tr>
-          <td>{country}</td>
+          <td>{country.country}</td>
           <td>
-            <strong>{numeral(cases).format(0, 0)}</strong>
+            <strong>{numeral(country.cases).format(0, 0)}</strong>
+          </td>
+          <td>
+            <strong>{numeral(country.recovered).format(0, 0)}</strong>
+          </td>
+          <td>
+            <strong>{numeral(country.deaths).format(0, 0)}</strong>
           </td>
         </tr>
       ))}
